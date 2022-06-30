9f0169ef9ef64e5640163c5af5aeb45be73e19a5
Art-Ndiema <art.ndiema@gmail.com>
2022-06-30 16:30:35 -0700

    Tudum

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
 /**
+ *
  *  * main - Entry point
- *   * print word to output
- *    * Return: Always 0 (Success)
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
  *     */
-
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

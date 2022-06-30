86e1a15c89dde04cc94e46a47b0ce1ef9f437c39
Art-Ndiema <art.ndiema@gmail.com>
2022-06-30 16:17:15 -0700

    Tudum

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - Starting point
+ *
+ *   * Description: prints (using puts function) "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
+ *     */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);

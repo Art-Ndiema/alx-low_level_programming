052e0d21582ac41a7381cde375a322303537ca7e
Art-Ndiema <art.ndiema@gmail.com>
2022-07-01 15:41:50 -0700

    Hey 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  main - main block
+ * Description: Use `putchar` function to print alphabets.
+ * Return: 0
+ */
+int main (void)
+	char x = 'a';
+	while (x <= 'z')
+{
+	putchar(x);
+x++
+}
+putchar('\n');
+return (0);
+}

b16a7530228160306370e2708ad041e5e18140ef
Art-Ndiema <art.ndiema@gmail.com>
2022-07-01 18:02:37 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 /**
 * main - main block
 * Description: Print all possible different combinations of 3 digits.
- Numbers must be separated by commas and a space.
+* Numbers must be separated by commas and a space.
 * The 3 digits must be different.
 * 012, 120, 102, 021, 201, 210 are considered the same combination.
 * print only the smallest combination of 3 digits.

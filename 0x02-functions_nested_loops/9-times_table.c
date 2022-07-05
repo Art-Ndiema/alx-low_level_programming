98cd39cf072a847eefbea916ed9f7ee631963b3b
Art-Ndiema <art.ndiema@gmail.com>
2022-07-05 15:34:39 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,7 @@
 */
 
 void times_table(void)
-
 {
-
 	int a;
 
 	int b;
@@ -63,27 +61,18 @@ void times_table(void)
 			else
 
 			{
-
 				_putchar ((c / 10) + '0');
 
 				_putchar ((c % 10) + '0');
 
 				if (b < 9)
-
 				{
-
 					_putchar(',');
 
 					_putchar (' ');
-
 				}
-
 			}
-
 		}
-
 		_putchar ('\n');
-
 	}
-
 }

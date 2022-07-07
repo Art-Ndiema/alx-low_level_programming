649c194a8782db0c30b6b7096d41fb7bb805be02
Art-Ndiema <art.ndiema@gmail.com>
2022-07-07 13:04:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,9 +2,9 @@
 
 /**
  * more_numbers - function that prints 10 times the numbers, from 0 to 14,
- * followed by a new line.
+ *followed by a new line.
  *
- * Return: Always 0 (Success).
+ *Return: Always 0 (Success).
 */
 
 void more_numbers(void)

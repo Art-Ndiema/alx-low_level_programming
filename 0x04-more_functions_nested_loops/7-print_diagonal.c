0f93f909d486695c857c75df26859f7ba3f63f0b
Art-Ndiema <art.ndiema@gmail.com>
2022-07-07 13:52:26 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,45 +10,19 @@ int i;
 int b;
 
 if (n > 0)
-
 {
-
 	for (i = 0; i < n; i++)
-
 	{
-
 		for (b = 0; b < i; b++)
-
 		{
-
 			_putchar (' ');
-
 		}
-
 		_putchar (92);
-
 		_putchar ('\n');
-
-
-
 	}
-
-
-
 }
-
-
-
-
-
 else
-
 {
-
 	_putchar ('\n');
-
 }
-
-
-
 }
